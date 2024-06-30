a6453330c26186a9ba133bb6dd233651647eb3bd
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-06-30 09:01:16 +0300

    “task1”

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: './js/dashboard_main.js',
+  mode: 'production',
+  entry: {
+    main: path.resolve(__dirname, './js/dashboard_main.js'),
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
