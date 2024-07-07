dd53b142d433403d910d75ecab37d9a78a604628
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-07-07 12:40:36 +0300

    “react”

---
+++ 0x02-react_props/task_0/dashboard/src/Header/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Header.css';
+import logo from "../assets/holberton-logo.jpg";
+
+export default function Header() {
+  return (
+    <>
+
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+
+    </>
+
+
+  )
+};
---
+++ 0x02-react_props/task_0/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Notifications.css";
+import closeIcon from "../assets/close-icon.png";
+import { getLatestNotification } from "../utils/utils";
+
+function Notifications() {
+  const handleClick = () => {
+    console.log("Close button has been clicked");
+  };
+  return (
+    <div className="Notifications">
+      <button
+        aria-label="Close"
+        style={{
+          background: "none",
+          border: "none",
+          position: "absolute",
+          right: "10px",
+          top: "12px",
+          cursor: "pointer",
+        }}
+        onClick={handleClick}
+      >
+        <img src={closeIcon} alt="close-icon" />
+      </button>
+      <p>Here is the list of notifications</p>
+      <ul>
+        <li data="default">New course available</li>
+        <li data="urgent">New resume available</li>
+        <li
+          data="urgent"
+          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+        ></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Notifications;
