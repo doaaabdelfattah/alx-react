aad0e63115e61425c3b94ab980b45a59b1669fb8
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-07-09 13:20:13 +0300

    task[0]

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,13 +1,12 @@
-import React from "react";
+import React, { Component } from "react";
 import Notifications from "../Notifications/Notifications";
 import Header from "../Header/Header";
 import Login from "../Login/Login";
 import Footer from "../Footer/Footer";
 import "./App.css";
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import CourseList from "../CourseList/CourseList";
 import { getLatestNotification } from "../utils/utils";
-
 
 const listCourses = [
   { id: 1, name: "ES6", credit: 60 },
@@ -15,34 +14,40 @@ const listCourses = [
   { id: 3, name: "React", credit: 40 },
 ];
 
-
 const listNotifications = [
   { id: 1, type: "default", value: "New course available" },
   { id: 2, type: "urgent", value: "New resume available" },
   { id: 3, type: "urgent", html: getLatestNotification() },
 ];
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    // State initialization if needed
+  }
 
-function App({ isLoggedIn }) {
-  return (
-    <React.Fragment>
-      <Notifications listNotifications={listNotifications} />
-      <div className="App">
-        <Header />
-        {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
-        <Footer />
-      </div>
-    </React.Fragment>
-  );
+  static propTypes = {
+    isLoggedIn: PropTypes.bool,
+  };
 
-}
-App.prototypes = {
-  isLoggedIn: propTypes.bool,
-}
+  static defaultProps = {
+    isLoggedIn: false,
+  };
 
+  render() {
+    const { isLoggedIn } = this.props;
 
-App.defaultProps = {
-  isLoggedIn: false,
+    return (
+      <React.Fragment>
+        <Notifications listNotifications={listNotifications} />
+        <div className="App">
+          <Header />
+          {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+          <Footer />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
