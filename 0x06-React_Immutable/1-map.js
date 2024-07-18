6e8ff9416d91b8e5d37023309a0ae3a0db8b87a2
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-07-19 00:00:28 +0300

    [task1]

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -2,13 +2,4 @@ import { Map } from 'immutable';
 
 export default function getImmutableObject(object) {
   return Map(object);
-}
-
-objData = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132
-}
-
-console.log(getImmutableObject(objData));
+};
